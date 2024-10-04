d433fe5f85144c6cb03f4d6373901890362b7e8c
Omar Harras <omar.harras@iken.tech>
2024-10-04 18:08:00 +0300

    add env file to store api key

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,20 +1,15 @@
-// /src/services/firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAIK_DRn56s0EWMpw2PKo0D38OyjjMbmHU',
-  authDomain: 'motors-website-6fd1e.firebaseapp.com',
-  projectId: 'motors-website-6fd1e',
-  storageBucket: 'motors-website-6fd1e.appspot.com',
-  messagingSenderId: '779192802535',
-  appId: '1:779192802535:web:6f47dbbc42a38160489642',
-  measurementId: 'G-M09KCLE4RD',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 export const db = getFirestore(app);
