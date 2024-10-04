096fe7470d11b2ebcc5b0a5500a04b3aeacac834
Omar Harras <omar.harras@iken.tech>
2024-10-04 15:36:23 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className='w-full h-44 bg-red-300 flex justify-center items-center text-3xl font-bold tracking-wider'>omar</div>;
+}
+
+export default App;
